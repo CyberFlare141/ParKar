3abb312e1891aeece6c77e45f160816902542319
Masrafi Iqbal <masrafiiqbal@gmail.com>
2026-02-20 23:33:27 +0600

    Database and Frontend setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/DashBoard" element={<Dashboard />} />
+
+    </Routes>
+  </BrowserRouter>
+);
