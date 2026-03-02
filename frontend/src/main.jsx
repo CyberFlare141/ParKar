ceaf7851f3dad69143e93579a455e2f539d5ddba
Masrafi Iqbal <masrafiiqbal@gmail.com>
2026-03-02 23:37:26 +0600

    Add initial project pages with demo content

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/DashBoard" element={<Dashboard />} />
-
-    </Routes>
+    <App />
   </BrowserRouter>
 );
