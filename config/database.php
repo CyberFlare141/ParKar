bbe3b784bc210226e7abccd165e5c91a21eab14f
Masrafi Iqbal <masrafiiqbal@gmail.com>
2026-04-18 11:08:07 +0600

    Configure Laravel to use the Certificate

--- config/database.php
+++ config/database.php
@@ -60,6 +60,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => filter_var(env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', true), FILTER_VALIDATE_BOOL),
             ]) : [],
         ],
 
@@ -75,7 +76,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => array_filter([
+                'sslrootcert=' . env('DB_SSLROOTCERT', ''),
+            ], fn ($option) => $option !== 'sslrootcert='),
         ],
 
         'sqlsrv' => [
